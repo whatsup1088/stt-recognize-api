ea8725c13d5edfb8f5b30318f40e4fb48289ac08
lianglala0423 <lianglala0423@gmail.com>
2021-03-02 22:45:56 -0500

    [fix]use redis rpush to save, refactor send_action

--- NLU/nlu.py
+++ NLU/nlu.py
@@ -1,19 +1,15 @@
-import requests
-from flask import Flask, request
-from urllib.parse import quote
-
 import redis
 
 
-app = Flask(__name__)
-
 textbook = ''
 state = 0
+request_id = 'ivr'
 
 
 def save_to_redis(request_id, nlu_rslt):
     r = redis.StrictRedis(host="192.168.8.166", port=6379, db=0, password='crv1313')
-    r.set(request_id, nlu_rslt)
+    r.rpush(request_id, nlu_rslt)
+    r.expire(request_id, 2*3600)
 
 
 def in_sentence(keywords, sentence):
@@ -26,46 +22,62 @@ def in_sentence(keywords, sentence):
 
 def send_action(textbook, state):
     if state == -1:
-        return 'close'
+        next_action = 'close'
     
     if state == 0:
         if in_sentence(['信用卡', '卡', '卡片'], textbook):
             state = 1
-            return 'F01'
+            next_action = 'F01'
         if in_sentence(['存款', '貸款', '轉帳'], textbook):
             state = 2
-            return 'F02'
-        return 'retry'
+            next_action = 'F02'
+        else:
+            next_action = 'retry'
 
     if state == 1:
         if in_sentence(['帳單', '繳費'], textbook):
-            return 'A01'
+            state = 0
+            next_action = 'A01'
         if in_sentence(['停卡', '掛失', '開卡'], textbook):
-            return 'A02'
+            state = 0
+            next_action = 'A02'
         if in_sentence(['卡友貸', '貸款' '刷卡分期'], textbook):
-            return 'A03'
+            state = 0
+            next_action = 'A03'
         if in_sentence(['紅利', '機場服務兌換', '機場接送', '旅遊保險'], textbook):
-            return 'A04'
+            state = 0
+            next_action = 'A04'
         if in_sentence(['機場接送'], textbook):
-            return 'A05'
-        return 'retry'
+            state = 0
+            next_action = 'A05'
+        else:
+            next_action = 'retry'
 
     if state == 2:
         if in_sentence(['帳戶查詢', '傳真'], textbook):
-            return 'A06'
+            state = 0
+            next_action = 'A06'
         if in_sentence(['轉帳', '定存'], textbook):
-            return 'A07'
+            state = 0
+            next_action = 'A07'
         if in_sentence(['掛失', '密碼變更'], textbook):
-            return 'A08'
+            state = 0
+            next_action = 'A08'
         if in_sentence(['貸款'], textbook):
-            return 'A09'
+            state = 0
+            next_action = 'A09'
         if in_sentence(['簽帳'], textbook):
-            return 'A10'
+            state = 0
+            next_action = 'A10'
         if in_sentence(['外匯'], textbook):
-            return 'A11'
-        return 'retry'
+            state = 0
+            next_action = 'A11'
+        else:
+            next_action = 'retry'
 
-    return 'retry'
+    save_to_redis(request_id, next_action)
+
+    return state
 
 
 if __name__ == "__main__":
